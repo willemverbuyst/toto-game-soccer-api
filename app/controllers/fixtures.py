fcff5ca29119f55b8ee306888d44dceb61ab238c
Willem Verbuyst <willemverbuyst@gmail.com>
2022-11-30 05:05:45 +0100

    Handle unhappy path

--- app/controllers/fixtures.py
+++ app/controllers/fixtures.py
@@ -1,22 +1,26 @@
 import os
 from fastapi import APIRouter
 from typing import Union
-from fastapi import Header
+from fastapi import Header, HTTPException, status
 from dotenv import load_dotenv
 from business.fixtures import get_fixtures_from_json
 from business.validators import validate_api_key
 load_dotenv()
 
-fixtures_router = APIRouter()
-
 league_id = os.getenv("LEAGUE_ID")
+
+fixtures_router = APIRouter()
 
 
 @fixtures_router.get(f"/fixtures/league/{league_id}", tags=["fixtures"])
 async def get_fixtures(toto_game_API_Key: Union[str, None] = Header(default=None)) -> dict:
+    """Get all fixtures for league"""
     valid = validate_api_key(toto_game_API_Key)
     if not valid:
-        return {"message": "unauthorized"}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect api key"
+        )
 
     fixtures = get_fixtures_from_json()
     return {"api": {"fixtures": fixtures}}
--- app/controllers/teams.py
+++ app/controllers/teams.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from typing import Union
-from fastapi import Header
+from fastapi import Header, HTTPException, status
 from business.teams import get_teams_from_json
 from business.validators import validate_api_key
 
@@ -9,9 +9,13 @@ teams_router = APIRouter()
 
 @teams_router.get("/teams/search/Netherlands", tags=["teams"])
 async def get_teams(toto_game_API_Key: Union[str, None] = Header(default=None)) -> dict:
+    """Get all teams from the Netherlands"""
     valid = validate_api_key(toto_game_API_Key)
     if not valid:
-        return {"message": "unauthorized"}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect api key"
+        )
 
     teams = get_teams_from_json()
     return {"api": {"teams": teams}}
